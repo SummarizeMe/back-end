b48abd64e251a8898369da375bfa32a29f6bfb48
min1018 <meet214ba@naver.com>
2023-01-20 19:00:14 +0900

    first commit

---
+++ src/main/java/SumMe/SummerizeMe/SummerizeMeApplication.java
@@ -0,0 +1,13 @@
+package SumMe.SummerizeMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerizeMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerizeMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SumMe/SummerizeMe/SummerizeMeApplicationTests.java
@@ -0,0 +1,13 @@
+package SumMe.SummerizeMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerizeMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
